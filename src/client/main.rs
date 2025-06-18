8e6711f6ab0aeaff2456b9a9e3d215eab5b2868c
WingedSeal <firm09719@gmail.com>
2025-06-18 18:36:55 +0700

    feat: add handler to ensure correctly framed InputEvent

--- src/client/main.rs
+++ src/client/main.rs
@@ -13,6 +13,13 @@ fn main() {
             continue;
         };
         let encoded = postcard::to_allocvec(&key_event).unwrap();
+        let size = encoded.len();
+        if size > u8::MAX.into() {
+            panic!("Encoded InputEvent should never be larger than 255 bytes.");
+        }
+        if let Err(err) = stream.write_all(&(size as u8).to_be_bytes()) {
+            println!("Failed: {}", err)
+        }
         if let Err(err) = stream.write_all(&encoded) {
             println!("Failed: {}", err)
         };
--- src/server/main.rs
+++ src/server/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
 fn handle_stream(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
+    let mut stream_read_handler = StreamReadHandler::new();
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => {
@@ -19,7 +20,7 @@ fn handle_stream(mut stream: TcpStream) {
                 break;
             }
             Ok(bytes_read) => {
-                handle_stream_read(&buffer[..bytes_read]);
+                stream_read_handler.read_buffer(&buffer[..bytes_read]);
             }
             Err(e) => {
                 println!("Read error: {}", e);
@@ -29,7 +30,58 @@ fn handle_stream(mut stream: TcpStream) {
     }
 }
 
-fn handle_stream_read(buffer: &[u8]) {
-    let key_event: InputEvent = postcard::from_bytes(&buffer).unwrap();
-    dbg!(key_event);
+struct StreamReadHandler {
+    size_prefix: Option<usize>,
+    /// Buffer for InputEvent. It's length can never exceed size_prefix which is sent through TCPStream.
+    input_event_buffer: Vec<u8>,
+}
+
+impl StreamReadHandler {
+    pub fn new() -> StreamReadHandler {
+        StreamReadHandler {
+            size_prefix: None,
+            input_event_buffer: vec![],
+        }
+    }
+    pub fn read_buffer(&mut self, buffer: &[u8]) {
+        let mut buffer_index = 0;
+        while buffer_index < buffer.len() {
+            match self.size_prefix {
+                None => {
+                    self.size_prefix = Some(buffer[buffer_index].into());
+                    // There'll be at least 1 byte in the buffer. Since bytes_read cannot be 0.
+                    debug_assert!(
+                        self.input_event_buffer.len() == 0,
+                        "input_event_buffer should be cleared."
+                    );
+                    buffer_index += 1;
+                }
+                Some(size_prefix) => {
+                    let available_space = size_prefix - self.input_event_buffer.len();
+                    let will_buffer_be_full = buffer.len() - buffer_index >= available_space;
+                    if will_buffer_be_full {
+                        self.input_event_buffer.extend_from_slice(
+                            &buffer[buffer_index..(buffer_index + available_space)],
+                        );
+                        buffer_index += available_space;
+                        self.input_event_buffer_full();
+                    } else {
+                        self.input_event_buffer
+                            .extend_from_slice(&buffer[buffer_index..]);
+                    }
+                }
+            };
+        }
+    }
+    fn input_event_buffer_full(&mut self) {
+        let input_event: InputEvent = postcard::from_bytes(&self.input_event_buffer)
+            .expect("Handler ensures complete and correctly framed InputEvent");
+        self.input_event_buffer.clear();
+        self.size_prefix = None;
+        handle_input_event(input_event);
+    }
+}
+
+fn handle_input_event(input_event: InputEvent) {
+    dbg!(input_event);
 }
