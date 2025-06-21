1a9fb4f7fdb4422739223956bfeb9c9dd117227d
WingedSeal <firm09719@gmail.com>
2025-06-21 22:38:37 +0700

    feat: add docs to the server/main.rs

--- src/server/main.rs
+++ src/server/main.rs
@@ -10,7 +10,8 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use std::{fmt, thread};
 
-const TIMEOUT_DURATION: Duration = Duration::from_secs(2);
+/// How long should the handler wait until it process InputEvents
+const DEBOUNCE_DURATION: Duration = Duration::from_secs(2);
 
 fn main() {
     if std::env::var_os("RUST_LOG").is_none() {
@@ -37,6 +38,7 @@ fn main() {
                 };
                 info!("Client connected: {}", &peer_addr);
                 let stdout = stdout_mutex.clone();
+                // Each TCP connection gets its own dedicated thread and StreamReadHandler
                 thread::spawn(move || {
                     handle_stream(stream, stdout, peer_addr);
                 });
@@ -57,10 +59,21 @@ fn main() {
     }
 }
 
+/// Handle TCPStream
+///
+/// Create a buffer for TCP, read from TCPStream. This function should get its own thread.
+///
+/// # Arguments
+///
+/// * `stream` - TCPStream to handle
+/// * `stdout` - A mutex lock used to avoid race condition for normal println
+/// * `peer_addr` - Peer address from `stream`. This is only passed through to avoid getting it
+/// from stream again.
 fn handle_stream(mut stream: TcpStream, stdout: Arc<Mutex<Stdout>>, peer_addr: String) {
     let mut buffer = [0; 1024];
     let mut stream_read_handler = StreamReadHandler::new(stdout.clone(), peer_addr);
-    if let Err(e) = stream.set_read_timeout(Some(TIMEOUT_DURATION)) {
+    // Read Timeout is used for debouncing handling input events.
+    if let Err(e) = stream.set_read_timeout(Some(DEBOUNCE_DURATION)) {
         error!("Failed to set read timeout: {}", e);
         return;
     }
@@ -74,6 +87,8 @@ fn handle_stream(mut stream: TcpStream, stdout: Arc<Mutex<Stdout>>, peer_addr: S
                 if let Err(err) = stream_read_handler.read_buffer(&buffer[..bytes_read]) {
                     warn!("Failed to read buffer: {}", err);
                     warn!("Shutting down the connection");
+                    // Since TCP guarantee reliability, failing to read buffer can only mean an
+                    // attack from a threat actor. Hence stream doesn't finish gracefully.
                     drop(stream);
                     return;
                 }
@@ -93,15 +108,25 @@ fn handle_stream(mut stream: TcpStream, stdout: Arc<Mutex<Stdout>>, peer_addr: S
 }
 
 struct StreamReadHandler {
+    /// First byte sent through TCP. Used for dictating how byte the following InputEvent postcard will take
     size_prefix: Option<u8>,
-    /// Buffer for InputEvent. It's length can never exceed size_prefix which is sent through TCPStream.
+    /// Buffer to contain postcard byte representation of InputEvent. It's length can never exceed size_prefix which is sent through TCPStream.
     input_event_buffer: Vec<u8>,
+    /// List of InputEvents to handle all at once during debouncing
     input_events: Vec<InputEvent>,
+    /// Mutex Lock to avoid race condition when using println
     stdout: Arc<Mutex<Stdout>>,
+    /// Peer address of the stream it's handling
     peer_addr: String,
 }
 
 impl StreamReadHandler {
+    /// Create a new StreamReadHandler
+    ///
+    /// # Arguments
+    ///
+    /// * `stdout` - A mutex lock used to avoid race condition for normal println
+    /// * `peer_addr` - Peer address of the stream it's handling
     pub fn new(stdout: Arc<Mutex<Stdout>>, peer_addr: String) -> StreamReadHandler {
         StreamReadHandler {
             size_prefix: None,
@@ -111,13 +136,30 @@ impl StreamReadHandler {
             peer_addr,
         }
     }
+
+    /// Read a buffer and handle it accordingly
+    ///
+    /// If `size_prefix` is already set, keep putting bytes in buffer into `input_even_buffer`
+    /// until it's full then convert it to InputEvent, clear it and put the event into
+    /// `input_events`. Otherwise, set `size_prefix` to the first byte gotten.
+    /// If there's still bytes leftover in the buffer, repeat.
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` - TCP stream buffer to read and handle
+    ///
+    /// # Errors
+    ///
+    /// * `InvalidEncodedInputEventError` - If, at any point, InputEvent cannot be decoded properly
+    /// or the event does not make sense (contain non-keyboard event).
     pub fn read_buffer(&mut self, buffer: &[u8]) -> Result<(), InvalidEncodedInputEventError> {
         let mut buffer_index = 0;
         while buffer_index < buffer.len() {
             match self.size_prefix {
                 None => {
                     self.size_prefix = Some(buffer[buffer_index]);
-                    // There'll be at least 1 byte in the buffer. Since bytes_read cannot be 0.
+                    // There'll be at least 1 byte in the buffer. Since 0 bytes_read means client
+                    // disconnected, and this function should never be called.
                     debug_assert!(
                         self.input_event_buffer.len() == 0,
                         "input_event_buffer wasn't cleared."
@@ -147,6 +189,15 @@ impl StreamReadHandler {
         }
         Ok(())
     }
+    /// Called when `input_event_buffer` is full, converting it into InputEvent
+    ///
+    /// Use postcard to decode `input_event_buffer` and put the result InputEvent
+    /// into `input_events`. Then clear the buffer and reset `size_prefix`
+    ///
+    /// # Errors
+    ///
+    /// * `InvalidEncodedInputEventError` - If InputEvent is not a key event or encoded bytes is
+    /// invalid.
     fn input_event_buffer_full(&mut self) -> Result<(), InvalidEncodedInputEventError> {
         self.input_events.push(
             match postcard::from_bytes::<InputEvent>(&self.input_event_buffer) {
@@ -172,9 +223,13 @@ impl StreamReadHandler {
         Ok(())
     }
 
+    /// Handle multiple InputEvents at once when there's nothing coming through TCPStream for a
+    /// while (timeout).
+    ///
+    /// Parse InputEvents into human-readable text then print it out and clear the `input_events`.
     fn handle_input_events(&mut self) {
-        let input_events = std::mem::take(&mut self.input_events);
-        let text = input_events_to_text(&input_events);
+        let text = input_events_to_text(&self.input_events);
+        self.input_events.clear();
         let _guard = self
             .stdout
             .lock()
